f40d84c0fb9ee7cdfe1a6ed0480c14e223360ff6
Jeremy Parker <jrobinparker@gmail.com>
2018-07-03 01:47:47 +0000

    automatically backed up by learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,8 +2,6 @@ class AppointmentsController < ApplicationController
   
   def show
     @appointment = Appointment.find(params[:id])
-    @patient = Patient.find(params[:patient_id])
-    @doctor = Doctor.find(params[:doctor_id])
   end
   
 end
